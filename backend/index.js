b3728d833104210c50b182484110b9264e5ad7ac
Alisson <alissonrocio@hotmail.com>
2021-09-15 08:53:27 -0300

    Hello World do Express

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,12 @@
-console.log('Olá Mundo')
+import express from 'express'
+
+const app = express()
+const port = 8087
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
